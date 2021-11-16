49bdf12f603c91f20b21ed9df902b6ef1785b0bb
HANY DESOKI <88154293+sourisaboule@users.noreply.github.com>
2021-11-16 14:01:11 +0100

    Update feature_engineering.py

--- data_science/feature_engineering.py
+++ data_science/feature_engineering.py
@@ -13,3 +13,22 @@ def distance_coords(m1, m2, r=6371):
 
     return 2 * r * math.asin(
         math.sqrt(math.sin((phi2 - phi1) / 2) ** 2 + math.cos(phi1) * math.cos(phi2) * math.sin((lam2 - lam1) / 2) ** 2))
+
+def convert_kj_kcal(e_kj):
+    '''Convert kilojoule to kilocal'''
+    e_kcal = e_kj/4.184
+    return e_kcal
+
+def weights_sum(values, weights=None):
+    '''Return sum of values with weights'''
+
+    if weights is None:
+        return sum(values)
+
+    a = len(values)
+    b = len(weights)
+
+    if a != b:
+        raise IndexError(f'values ({a}) and weights ({b}) must be same lenght')
+
+    return sum([v * w for v, w in zip(values, weights)])
